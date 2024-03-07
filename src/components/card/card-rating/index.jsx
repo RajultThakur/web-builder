97d51d0b65a2b2db8a6153167a68706969dfd39a
Rajul Thakur <thakurajul8888@gmail.com>
2024-03-07 15:58:50 +0530

    added card's label and rating components

---
+++ src/components/card/card-rating/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactStars from "react-rating-stars-component";
+
+function RatingCard({rating, ratingType}) {
+  return (
+    <div className="flex items-center flex-col">
+          <p>{rating}</p>
+          <p>{ratingType}</p>
+          <ReactStars
+                        count={5}
+                        value={rating}
+                        isHalf={true}
+                        // onChange={ratingChanged}
+                        size={28}
+                        activeColor="#ffb700"
+                    />
+          </div>
+  )
+}
+
+export default RatingCard
