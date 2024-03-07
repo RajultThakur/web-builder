f12c6bb0f00d7ce55e8df9a57ace23fa34145011
Rajul Thakur <thakurajul8888@gmail.com>
2024-03-07 15:54:14 +0530

    initilize project

---
+++ src/components/card/card-highlight/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Highlight from './Highlight'
+
+function HighlightCard({classes="", indexes=[], titles}) {
+  return (
+    <div className={`${classes}  flex items-start flex-col justify-around h-[116px]`}>
+        {indexes.map((index,ind) => {
+            return (
+                <Highlight key={ind} number = {index} title = {titles[ind]}/>
+            )
+        })}
+        </div>
+  )
+}
+
+export default HighlightCard
